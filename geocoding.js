52db1b6f165e27ea43d944fb108ba83c8764f9f5
Michael Nelson <michael.nelson@fulbrightmail.org>
2016-10-21 10:46:10 -0400

    fix json formatting error

--- geocoding.js
+++ geocoding.js
@@ -1,2 +1,7 @@
 var data = require('./data.json');
+var MapboxClient = require('mapbox');
+var client = new MapboxClient('pk.eyJ1IjoibWljaGFlbG5ldCIsImEiOiItbWFqc3BvIn0.iuqIsFSNd5Ut-5_1W4RR_g');
+client.geocodeForward('Chester, NJ', function(err, res) {
+  console.log(res);
+});
 console.log(data);
